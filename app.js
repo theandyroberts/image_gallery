3271145e4f990d1c41855f6e5e23d76ab3f5ac5e
Andy Roberts <theandyroberts@note15.com>
2024-03-14 03:30:57 +0000

    organize files and add url to api call

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 6060; // Port number for the server
+const jsonFilesDirectory = path.join(__dirname, '/json-files'); 
+
+// Use CORS middleware to allow cross-origin requests
+app.use(cors());
+
+// webroot path
+app.get('/', (req, res) => {
+  // Construct the absolute path to the gallery.html file
+  const absolutePath = path.join(__dirname, 'public', 'gallery.html');
+  
+  // Send the file as the response
+  res.sendFile(absolutePath);
+});
+
+// static file serving
+app.use(express.static('public'));
+
+// Endpoint to list all JSON files
+app.get('/api/read-files', (req, res) => {
+  fs.readdir(jsonFilesDirectory, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return res.status(500).send('Error reading directory');
+    }
+
+    console.log(`fetching files...`);
+    const jsonFiles = files.filter(file => file.endsWith('.json'));
+    res.json(jsonFiles);
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
